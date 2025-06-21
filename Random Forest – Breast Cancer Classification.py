e16df76bb3d42c6c3ad148cf1f91a345b40470da
GowthamGoda99 <btechman.vip1@gmail.com>
2025-06-21 11:11:38 +0530

    Add files via upload

---
+++ Random Forest – Breast Cancer Classification.py
@@ -0,0 +1,31 @@
+# MEDIUM – 01. Random Forest – Breast Cancer Classification
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = load_breast_cancer()
+X, y = data.data, data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+y_prob = model.predict_proba(X_test)[:, 1]
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
+print("ROC AUC:", roc_auc_score(y_test, y_prob))
+
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d")
+plt.title("Confusion Matrix")
+plt.show()
+
+from sklearn.metrics import RocCurveDisplay
+RocCurveDisplay.from_estimator(model, X_test, y_test)
+plt.title("ROC Curve")
+plt.show()
